8d6893e33d51afc0f66f9796888de815b1c3d64a
Josh Radack <josh.radack@gmail.com>
2023-08-22 10:35:29 -0400

    Calculated tenure length for each player

--- scripts/00-data_clean.r
+++ scripts/00-data_clean.r
@@ -39,7 +39,7 @@ roster_files <- list.files(roster_folder_path)
 roster_info_raw <- lapply(roster_files, function(x) read_ros(roster_folder_path, x)) |>
   bind_rows()
 
-# Merge team data with roster data and perform cleaning
+# Merge team and player data with roster data and perform cleaning
 roster_info <- roster_info_raw |>
   arrange(PLAYERID, YEAR, TEAM) |>
   left_join(
@@ -53,5 +53,26 @@ roster_info <- roster_info_raw |>
   ) |> 
   filter(
     !(LEAGUE == "" | is.na(LEAGUE))
+  ) |>
+  left_join(
+    bio |>
+      select(PLAYERID, LAST, FIRST, NICKNAME),
+    by = "PLAYERID"
   )
 
+# Calculate cumulative tenure with each team
+tenure_data <- roster_info |>
+  group_by(PLAYERID) |>
+  mutate(
+    # Tracks whether a player switched teams
+    changed_team = TEAM != lag(TEAM, default = first(TEAM)),
+    # Creates a new team name to differentiate separte stints with same team
+    team_stint = paste(TEAM, cumsum(changed_team) + 1)
+  ) |>
+  group_by(PLAYERID, team_stint) |>
+  mutate(
+    count = row_number()
+  )
+
+# Save dataset
+saveRDS(tenure_data, "data/cleaned/tenure_data.rds")
